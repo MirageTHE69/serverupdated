07b9ef3ee68103fb18e0e1d8a9e74d8fb5276ec5
unknown <mthakore12@gmail.com>
2022-06-29 14:19:05 +0530

    initira commit

---
+++ server.js
@@ -0,0 +1,27 @@
+// Import modules
+const
+    mongoose = require('mongoose'),
+    forest = require('forest-express-mongoose'),
+    dotenv = require('dotenv');
+
+// Load environment variables
+dotenv.config({ path: './config.env' });
+
+   
+
+// Connect to mongoDB cluster
+mongoose
+    .connect(process.env.DB_CONNECT, {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    })
+    .then(() => console.log('DB connection successful'));
+
+// Import main express application
+const app = require('./app');
+
+// Set up the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening at port ${PORT}`));
